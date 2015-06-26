259d1c60ff6cfb4b06641871791ec8956877d779
Johannes Sjölund <j.sjolund@gmail.com>
2015-06-27 00:22:06 +0200

    Standard libGDX project files

---
+++ desktop/src/nbody/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nbody.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nbody.NBody;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NBody(), config);
+	}
+}
